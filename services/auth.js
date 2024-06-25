5f49ee393a94f920b6d5b7777c84a20529ecf4d6
Joshua <akinleyejoshua.dev@gmail.com>
2024-06-25 02:47:51 +0100

    Update auth.js

--- services/auth.js
+++ services/auth.js
@@ -2,6 +2,6 @@ import { request } from "@/utils/axios";
 
 export const signupAPI = async (data) => request.post("/sign-up", data);
 
-export const signinAPI = async (data) => request.options("/sign-in", data);
+export const signinAPI = async (data) => request.post("/sign-in", data);
 
 export const recoverAPI = async (data) => request.post("/recover", data);
