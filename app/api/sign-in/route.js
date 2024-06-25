f9198ff7cc3e6ad7934fc2823d0723e830aeea42
Joshua <akinleyejoshua.dev@gmail.com>
2024-06-25 02:45:49 +0100

    Update route.js

--- app/api/sign-in/route.js
+++ app/api/sign-in/route.js
@@ -12,18 +12,7 @@ export const OPTIONS = async (req) => {
     if (userExist.pwd === pwd) {
       return new NextResponse(JSON.stringify({ ...userExist })).status(200);
     } else {
-      return new NextResponse.json(
-        { msg: "wrong-data" },
-        {
-          status: 200,
-          headers: {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-            "Access-Control-Allow-Headers": "Content-Type, Authorization",
-          },
-        }
-      );
-      // return new NextResponse(JSON.stringify({ msg: "wrong-data" })).status(200);
+      return new NextResponse(JSON.stringify({ msg: "wrong-data" })).status(200);
     }
   } else {
     return new NextResponse(JSON.stringify({ msg: "not-found" })).status(200);
