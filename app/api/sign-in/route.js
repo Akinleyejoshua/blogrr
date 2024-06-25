44240a946560e191c5e0eb29495179950833e37b
Joshua <akinleyejoshua.dev@gmail.com>
2024-06-25 03:09:32 +0100

    Update route.js

--- app/api/sign-in/route.js
+++ app/api/sign-in/route.js
@@ -4,20 +4,19 @@ import { NextResponse } from "next/server";
 export const POST = async (req) => {
   const { email, pwd } = await req.json();
 
-  // const users = new UDB("users");
+  const users = new UDB("users");
 
-  // const userExist = await users.findOne({ email });
+  const userExist = await users.findOne({ email });
 
-  // if (userExist.msg == "found") {
-  //   if (userExist.pwd === pwd) {
-  //     return new NextResponse(JSON.stringify({ ...userExist })).status(200);
-  //   } else {
-  //     return new NextResponse(JSON.stringify({ msg: "wrong-data" })).status(200);
-  //   }
-  // } else {
-  //   return new NextResponse(JSON.stringify({ msg: "not-found" })).status(200);
-  // }
-
-    return new NextResponse(JSON.stringify({ msg: "wrong-data" })).status(200);
-  
+  if (userExist.msg == "found") {
+    if (userExist.pwd === pwd) {
+      return new NextResponse(
+        JSON.stringify({ ...userExist })
+      );
+    } else {
+      return new NextResponse(JSON.stringify({ msg: "wrong-data" }));
+    }
+  } else {
+    return new NextResponse(JSON.stringify({ msg: "not-found" }));
+  }
 };
