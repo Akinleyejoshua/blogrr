1e036919cee09b67fb1596acea1190f631af0db6
Joshua <akinleyejoshua.dev@gmail.com>
2024-06-27 11:00:39 +0100

    Update useAuth.jsx

--- hooks/useAuth.jsx
+++ hooks/useAuth.jsx
@@ -117,7 +117,7 @@ export const useAuth = () => {
                         handleState("msgType", "success");
                         save("login-id", JSON.stringify(data._id));
                         const lastVisit = get("prev-url");
-                        console.log(lastVisit)
+                        // console.log(lastVisit)
                         if (
                             lastVisit != null
                         ) {
