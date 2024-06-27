069a217218737c0d9aece3a335c3a04ab8d8b0f0
Joshua <akinleyejoshua.dev@gmail.com>
2024-06-27 19:48:43 +0100

    Update db.js

--- app/api/db.js
+++ app/api/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const options = {};
 mongoose.set("strictQuery", false);
 const MONGO_URI = "mongodb://127.0.0.1:27017/blogrr";
-const MONGO_URI_LIVE = "mongodb+srv://blogrr:blogrr1@cluster0.xyyujqk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const MONGO_URI_LIVE = "mongodb+srv://blogrr:blogrr1@cluster0.v9devjw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 const connect = async () => {
   await mongoose
