423c0115b481fd42eca14dba313730e991d41bfc
Joshua <akinleyejoshua.dev@gmail.com>
2024-06-27 20:55:32 +0100

    Add files via upload

--- services/publish.js
+++ services/publish.js
@@ -1,3 +1,4 @@
 import { request } from "@/utils/axios";
 
 export const publishPostAPI = async (data) => request.post("/publish-post", data);
+export const updatePublishedPostAPI = async (data) => request.post("/update-published-post", data);
