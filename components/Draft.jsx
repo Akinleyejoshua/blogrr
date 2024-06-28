31cb2fc2797a384f0540561000e00362af2b73e4
Joshua <akinleyejoshua.dev@gmail.com>
2024-06-28 21:08:09 +0100

    Update Draft.jsx

--- components/Draft.jsx
+++ components/Draft.jsx
@@ -1,7 +1,17 @@
 import { useEffect, useRef, useState } from "react";
 import { Space } from "./Space";
-import { FaArrowRight, FaBold, FaHeading, FaItalic, FaList, FaParagraph, FaUnderline } from "react-icons/fa6";
-import { removeFromString } from "@/utils/helpers";
+import {
+    FaArrowRight,
+    FaBold,
+    FaHeading,
+    FaImage,
+    FaItalic,
+    FaLink,
+    FaList,
+    FaParagraph,
+    FaUnderline,
+} from "react-icons/fa6";
+import { atlify, removeFromString, urlify } from "@/utils/helpers";
 
 export const Draft = ({ val, onChange }) => {
     const contentRef = useRef(null);
@@ -11,100 +21,103 @@ export const Draft = ({ val, onChange }) => {
     const [typing, setTyping] = useState(false);
     const [onPreview, setOnPreview] = useState(false);
     const [content, setContent] = useState("");
-    const [lastPoint, setLastPont] = useState(0);
+    const [lastPoint, setLastPont] = useState(null);
 
     useEffect(() => {
-
         if (val !== undefined) {
             setTyping(true);
             if (contentRef.current.innerHTML == "") {
-                contentRef.current.innerHTML += val
-
+                contentRef.current.innerHTML += val;
+                setContent(val);
             }
         }
-    }, [val])
+    }, [val]);
 
     const appendElementAtCursor = (element) => {
-        lastPoint.insertNode(element);
-        lastPoint.setStartAfter(element);
-        lastPoint.collapse(true);
+        try {
+            lastPoint.insertNode(element);
+            lastPoint.setStartAfter(element);
+            lastPoint.collapse(true);
 
-        const selection = window.getSelection();
-        selection.removeAllRanges();
-        selection.addRange(lastPoint);
-        contentRef.current.focus();
+            const selection = window.getSelection();
+            selection.removeAllRanges();
+            selection.addRange(lastPoint);
+            contentRef.current.focus();
+        } catch (error) {
 
-    }
+        }
+
+    };
 
     const handleFormatting = (type) => {
-
         setTyping(true);
+        let space = document.createElement("i");
+        space.innerHTML = "&nbsp;";
+        space.className = "cont"
 
         switch (type) {
             case "h":
-
                 let h = document.createElement("h3");
                 h.innerHTML = "``";
                 appendElementAtCursor(h);
-
                 break;
 
             case "p":
                 let p = document.createElement("p");
                 p.innerHTML = "``";
                 appendElementAtCursor(p);
-
                 break;
             case "i":
                 let i = document.createElement("i");
                 i.innerHTML = "``";
                 appendElementAtCursor(i);
+                appendElementAtCursor(space);
 
                 break;
             case "li":
                 let ul = document.createElement("ul");
                 let li = document.createElement("li");
-                ul.appendChild(li)
+                ul.appendChild(li);
                 li.innerHTML = "``";
                 appendElementAtCursor(ul);
-
                 break;
             case "b":
                 let b = document.createElement("b");
                 b.innerHTML = "``";
                 appendElementAtCursor(b);
+                appendElementAtCursor(space);
 
                 break;
             case "tag":
                 let tag = document.createElement("i");
-                let space = document.createElement("i")
-                space.innerHTML = " ``"
-                space.style.fontStyle = "normal"
-                tag.className = "tag"
+                tag.className = "tag";
                 tag.innerHTML = "``";
                 appendElementAtCursor(tag);
                 appendElementAtCursor(space);
                 break;
-
             case "u":
                 let u = document.createElement("u");
                 u.innerHTML = "``";
                 appendElementAtCursor(u);
+                appendElementAtCursor(space);
 
                 break;
+
             case "cont":
-                let cont = document.createElement("i")
-                cont.innerHTML = " ``"
-                cont.style.fontStyle = "normal"
-                cont.style.fontWeight = "normal"
-                cont.style.textDecoration = "none"
-                appendElementAtCursor(cont);
-                
+                appendElementAtCursor(space);
                 break;
-
-
         }
     };
+
+    const handleImage = (blob) => {
+        setTyping(true);
+        let img = document.createElement("img");
+        img.className = "content-img";
+        img.src = blob;
+        let space = document.createElement("br");
+        appendElementAtCursor(img);
+        appendElementAtCursor(space);        
+    }
 
     function saveCursorPosition(contentEditableElement) {
         const selection = window.getSelection();
@@ -116,36 +129,30 @@ export const Draft = ({ val, onChange }) => {
         }
     }
 
+
+
     const handleText = (e) => {
         const text = e.target.innerHTML;
-        const formatedText = removeFromString(text, "`");
+        const removeLiterals = removeFromString(text, "`");
+        const formatedText = removeLiterals;
+
         setContent(formatedText);
         onChange(formatedText);
-        if (text.length > 0) {
+
+        if (text.length >= 0) {
             setTyping(true);
         } else {
             setTyping(false);
         }
     };
 
-    useEffect(() => {
-        const updateContent = () => {
-            const linkRegex =
-                /((http|https):\/\/)?(wwww\.)?([^\s]+)(\.[^\s])*(\/[^\s]*)?/g;
-            let newContent = contentRef.current.innerHTML.replace(
-                linkRegex,
-                (url) => {
-                    return `<a href=${url}>${url}</a>`;
-                }
-            );
-        };
-
-        updateContent();
-    }, [contentRef]);
-
     return (
         <div className="draft">
-            <div className={`tools ${typing ? "visible" : "visible"}`} ref={toolbarRef}>
+
+            <div
+                className={`tools ${typing ? "visible" : "visible"}`}
+                ref={toolbarRef}
+            >
                 <div
                     className="format-btn pointer c-white"
                     onClick={() => handleFormatting("h")}
@@ -153,15 +160,12 @@ export const Draft = ({ val, onChange }) => {
                     <FaHeading />
                 </div>
 
-                {/* <Space val={".3rem"} /> */}
-
                 <div
                     className="format-btn pointer"
                     onClick={() => handleFormatting("p")}
                 >
                     <FaParagraph />
                 </div>
-                {/* <Space val={".3rem"} /> */}
 
                 <div
                     className="format-btn pointer"
@@ -169,7 +173,6 @@ export const Draft = ({ val, onChange }) => {
                 >
                     <FaItalic />
                 </div>
-                {/* <Space val={".3rem"} /> */}
 
                 <div
                     className="format-btn pointer"
@@ -177,7 +180,6 @@ export const Draft = ({ val, onChange }) => {
                 >
                     <FaList />
                 </div>
-                {/* <Space val={".3rem"} /> */}
 
                 <div
                     className="format-btn pointer"
@@ -185,7 +187,6 @@ export const Draft = ({ val, onChange }) => {
                 >
                     <FaBold />
                 </div>
-                {/* <Space val={".3rem"} /> */}
 
                 <div
                     className="format-btn pointer"
@@ -193,15 +194,30 @@ export const Draft = ({ val, onChange }) => {
                 >
                     <FaUnderline />
                 </div>
-                {/* <Space val={".3rem"} /> */}
+                <div
+                    className="format-btn pointer items-center"
 
+                ><div className="input-file flex items-center">
+                        <FaImage className="icon" />
+                        <input className="fit" type="file" onChange={(e) => {
+                            const file = new FileReader();
+                            try {
+                                file.readAsDataURL(e.target.files[0]);
+                                file.onload = () => handleImage(file.result);
+
+                            } catch {
+
+                            }
+                        }} />
+                    </div>
+
+                </div>
                 <div
                     className="format-btn pointer"
                     onClick={() => handleFormatting("tag")}
                 >
                     ``
                 </div>
-                {/* <Space val={".3rem"} /> */}
 
                 <div
                     className="format-btn pointer"
@@ -212,10 +228,9 @@ export const Draft = ({ val, onChange }) => {
             </div>
 
             <Space val={".3rem"} />
-            <div className={`textarea ${onPreview ? 'not-visible' : 'visible'}`}>
+            <div className={`textarea ${onPreview ? "not-visible" : "visible"}`}>
                 <div
-
-                    onBlur={e => saveCursorPosition(e.target)}
+                    onBlur={(e) => saveCursorPosition(e.target)}
                     contentEditable={true}
                     className="text"
                     onInput={(e) => handleText(e)}
@@ -227,21 +242,24 @@ export const Draft = ({ val, onChange }) => {
                 )}
             </div>
 
-            <div className={`textarea ${!onPreview ? 'not-visible' : 'visible'}`}>
+            <div className={`textarea ${!onPreview ? "not-visible" : "visible"}`}>
                 <div
                     className="text"
                     ref={previewRef}
-                    dangerouslySetInnerHTML={{ __html: content }}
+                    dangerouslySetInnerHTML={{ __html: atlify(urlify(content)) }}
                 ></div>
-
             </div>
 
             <Space val={".3rem"} />
 
             {onPreview ? (
-                <small className="pointer under" onClick={() => setOnPreview(false)}>Continue editing</small>
+                <small className="pointer under" onClick={() => setOnPreview(false)}>
+                    Continue editing
+                </small>
             ) : (
-                <small className="pointer under" onClick={() => setOnPreview(true)}>Preview</small>
+                <small className="pointer under" onClick={() => setOnPreview(true)}>
+                    Preview
+                </small>
             )}
         </div>
     );
