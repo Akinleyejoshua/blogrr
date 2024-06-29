c806509ae049a30182ea04392a96f7e0eea8af2f
Joshua <akinleyejoshua.dev@gmail.com>
2024-06-29 06:21:40 +0100

    Update route.js

--- app/api/update-published-post/route.js
+++ app/api/update-published-post/route.js
@@ -5,11 +5,10 @@ import Post from "../models/Post";
 db();
 
 export const POST = async (req) => {
-  const { _id, title, content, is_comment, main_post_id, id } =
+  const { _id, title, content, is_comment, id } =
     await req.json();
   const publish = await Post.findByIdAndUpdate(id, {
     user_id: _id,
-    main_post_id: main_post_id,
     title: title,
     content: content,
     is_comment: is_comment,
