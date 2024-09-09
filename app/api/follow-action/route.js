db340dd90c2e20d6dc38b70ac6990be3b3d6b10c
Joshua <akinleyejoshua.dev@gmail.com>
2024-09-09 21:29:14 +0100

    Update route.js

--- app/api/follow-action/route.js
+++ app/api/follow-action/route.js
@@ -1,56 +1,57 @@
 import db from "../db";
 import { NextResponse } from "next/server";
 import User from "../models/User";
+import Notification from "../models/Notification";
 
 db();
 
 export const POST = async (req) => {
   const { id, type, user_id } = await req.json();
+  // console.log(id, user_id)
 
   try {
     const followedUser = await User.findOne({ _id: id }).lean();
     const followingUser = await User.findOne({ _id: user_id }).lean();
-  
+
+    // console.log(type);
+
     if (type == "follow") {
-      const alreadyFollowed = followedUser.following.find(
-        (item) => (item == id)
-      );
+      const alreadyFollowed = followedUser.following.find((item) => item == id);
+      const alreadyFollowers = followedUser.followers.find((item) => item == id)
 
       // console.log(alreadyFollowed);
-    
-      if (alreadyFollowed == undefined) {
-        
+
+      if (alreadyFollowed == undefined && alreadyFollowers == undefined) {
         await User.findByIdAndUpdate(user_id, {
           following: [...followingUser.following, id],
         });
-  
+
         await User.findByIdAndUpdate(id, {
           followers: [...followedUser.followers, user_id],
         });
       }
-  
+
       return new NextResponse(JSON.stringify({ followed: true }));
     } else if (type == "un-follow") {
-      const removeFollowing = followedUser.following.filter((item) => item != user_id);
-  
+      const removeFollowing = followedUser.following.filter(
+        (item) => item != user_id
+      );
+
       const removeFollowers = followingUser.followers.filter(
         (item) => item !== id
       );
-  
+
       await User.findByIdAndUpdate(user_id, {
         following: [...removeFollowing],
       });
-  
+
       await User.findByIdAndUpdate(id, {
         followers: [...removeFollowers],
       });
-  
-  
+
       return new NextResponse(JSON.stringify({ un_followed: true }));
     }
-  } catch (err){
-
+  } catch (err) {
+    console.log(err);
   }
-
-  
 };
