8b9da9f5552896432e669636ef1a5bf79acd88f8
Joshua <akinleyejoshua.dev@gmail.com>
2024-09-18 18:29:12 +0100

    Update route.js

--- app/api/get-profile/route.js
+++ app/api/get-profile/route.js
@@ -1,21 +1,40 @@
 import db from "../db";
 import { NextResponse } from "next/server";
 import User from "../models/User";
+import Notification from "../models/Notification";
 
 db();
 
 export const POST = async (req) => {
-    const {username} = await req.json();
+    const {username, id} = await req.json();
 
     try {
         const findUser = await User.findOne({username: username}).lean();
+        const userWhoViewed = await User.findOne({_id: id}).lean();
 
         if (findUser){
+            if (findUser._id != userWhoViewed._id){
+                const newNotification = new Notification({
+                    timestamp: Date.now(),
+                    seen: false,
+                    msg: `${userWhoViewed.username} viewed your profile`,
+                    title: "",
+                    type: "profile",
+                    user_id: userWhoViewed._id,
+                    content: "",
+                    path: `/@${userWhoViewed.username}`,
+                    to: findUser._id,
+                  });
+        
+                  newNotification.save();
+            }
+           
             return new NextResponse(JSON.stringify({msg: "found", ...findUser}))
         } else {
             return new NextResponse(JSON.stringify({msg: "not-found"}));
         }
     } catch (err){
+        console.log(err)
         return new NextResponse(JSON.stringify({msg: "not-found"}));
 
     }
