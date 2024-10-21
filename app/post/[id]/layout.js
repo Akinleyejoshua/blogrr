26ad9ab7cedb12ced8cf80906c7c66abc8b6aa09
Joshua <akinleyejoshua.dev@gmail.com>
2024-10-21 04:09:07 +0100

    Create layout.js

---
+++ app/post/[id]/layout.js
@@ -0,0 +1,26 @@
+
+import StoreProvider from "@/redux/StoreProvider";
+import { getPostAPI } from "@/services/post";
+
+export async function generateMetadata({params}) {
+    const id = params.id;
+    const data = await getPostAPI({ id });
+
+    return {
+      title: "Blogrr Social App",
+      openGraph: {
+        description: data.data.content,
+        title: data.data.title
+      },
+    }; 
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <StoreProvider>{children}</StoreProvider>
+      </body>
+    </html>
+  );
+}
