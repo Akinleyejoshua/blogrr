7dd9aacf0cbf40d1175827584c8ef82f3cd67b1e
Joshua <akinleyejoshua.dev@gmail.com>
2024-11-01 23:03:15 +0100

    Update layout.js

--- app/post/[id]/layout.js
+++ app/post/[id]/layout.js
@@ -1,16 +1,18 @@
 
 import StoreProvider from "@/redux/StoreProvider";
 import { getPostAPI } from "@/services/post";
+import { formatNumber } from "@/utils/helpers:
 
 export async function generateMetadata({params}) {
     const id = params.id;
-  const data = await getPostAPI({ id });
-
+    const res = await getPostAPI({ id });
+    const data = res.data
     return {
       title: "Blogrr Social App",
       openGraph: {
-        description: data.data.content,
-        title: data.data.title,
+        description: data.content,
+        title: `${data.title} by ${data.username} · ${formatNumber(data.views.length)} Views · ${formatNumber(data.likes.length)} Likes · 
+        ${formatNumber(data.comments.length) Comments}`,
         images: ["https://blogrpro.vercel.app/icon.png"],
 
       },
