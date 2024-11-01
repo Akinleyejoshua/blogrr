24b53dd7e58380e0ec99b5f6a1a7428e0629bb0a
Joshua <akinleyejoshua.dev@gmail.com>
2024-11-02 00:40:39 +0100

    Update page.jsx

--- app/notifications/page.jsx
+++ app/notifications/page.jsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { Header } from "@/components/Header"
+import { Loader } from "@/components/Loader";
 import { NotificationBar } from "@/components/NotificationBar";
 import { SideBar } from "@/components/SideBar"
 import { Space } from "@/components/Space";
@@ -9,30 +10,36 @@ import { useUser } from "@/hooks/useUser"
 import { get } from "@/utils/localstorage";
 import { useEffect } from "react";
 
-export default function Page(){
-    const {getUserNotification, notifications, setNotificationSeenStatus} = useUser();
+export default function Page() {
+    const { getUserNotification, notifications, setNotificationSeenStatus, loading, } = useUser();
     const { authenticate } = useAuth();
 
     useEffect(() => {
         authenticate();
     }, [])
-    
+
+
+
     useEffect(() => {
         const userId = get("login-id")
+
         getUserNotification(userId)
         setNotificationSeenStatus(userId, true);
     }, [])
 
     return <main className="home notifications">
         <div className="flex row fit">
-            <SideBar/>
+            <SideBar />
             <div className="flex col fit">
-                <Header title={"Notifications"}/>
+                <Header title={"Notifications"} />
                 <div className="main scroll-y">
-                    <Space val={".3rem"}/>
-                    <div className="notification-section">
-                    <NotificationBar data={notifications}/>
-                    </div>
+                    <Space val={".3rem"} />
+                    {(loading) ? <Loader size={"1.3rem"} /> :
+                        <div className="notification-section">
+                            <NotificationBar data={notifications?.length > 0 ? notifications: []} />
+                        </div>
+                    }
+
                 </div>
             </div>
         </div>
