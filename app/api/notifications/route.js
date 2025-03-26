d8f9d3b9d46fe602b0aae3f643f46164010fbd82
Joshua <akinleyejoshua.dev@gmail.com>
2025-03-26 01:33:44 +0100

    Update route.js

--- app/api/notifications/route.js
+++ app/api/notifications/route.js
@@ -9,62 +9,68 @@ db();
 export const POST = async (req) => {
   const { action, user_id, seen, notif_id } = await req.json();
   if (action == "get") {
-
-    const data = []
-    const userNotifications = await Notification.find({ to:user_id }).lean();
+    const data = [];
+    const userNotifications = await Notification.find({ to: user_id }).lean();
     const users = await User.find().lean();
     const posts = await Post.find().lean();
 
     userNotifications.forEach((item, i) => {
-      
-      if (item.type == "like"){
-        const findPost = posts.find(id => id._id == item.content);
-        const findUser = users.find(id => id._id == item.user_id);
-        data.push({
-          ...item,
-          post: findPost,
-          user: findUser
-        })
-      }
-
-      if (item.type == "follow"){
-        const findUser = users.find(id => id._id == item.user_id);
-        data.push({
-          ...item,
-          user: findUser
-        })
-      }
-
-      if (item.type == "profile"){
-        const findUser = users.find(id => id._id == item.user_id);
-        data.push({
-          ...item,
-          user: findUser
-        })
-      }
-
-      if (item.type == "comment"){
-        const findPost = posts.find(id => id._id == (item.content));
-        const findUser = users.find(id => id._id == item.user_id);
+      if (item.type == "like") {
+        const findPost = posts.find((id) => id._id == item.content);
+        const findUser = users.find((id) => id._id == item.user_id);
         data.push({
           ...item,
           post: findPost,
           user: findUser,
-        })
+        });
       }
 
-      if (item.type == "signin"){
-        const findUser = users.find(id => id._id == item.user_id);
+      if (item.type == "follow") {
+        const findUser = users.find((id) => id._id == item.user_id);
         data.push({
           ...item,
           user: findUser,
-        })
+        });
       }
 
-    })
+      if (item.type == "profile") {
+        const findUser = users.find((id) => id._id == item.user_id);
+        data.push({
+          ...item,
+          user: findUser,
+        });
+      }
+
+      if (item.type == "tagged") {
+        const findPost = posts.find((id) => id._id == item.content);
+        const findUser = users.find((id) => id._id == item.user_id);
+        data.push({
+          ...item,
+          post: findPost,
+          user: findUser,
+        });
+      }
+      if (item.type == "comment") {
+        const findPost = posts.find((id) => id._id == item.content);
+        const findUser = users.find((id) => id._id == item.user_id);
+        data.push({
+          ...item,
+          post: findPost,
+          user: findUser,
+        });
+      }
+
+      if (item.type == "signin") {
+        const findUser = users.find((id) => id._id == item.user_id);
+        data.push({
+          ...item,
+          user: findUser,
+        });
+      }
+    });
     return new NextResponse(JSON.stringify(data || []));
   } else if (action == "seen") {
-    await Notification.find({to:user_id}).updateMany({seen: seen})
+    await Notification.find({ to: user_id }).updateMany({ seen: seen });
     return new NextResponse(JSON.stringify({ seen: true }));
   }
 };
